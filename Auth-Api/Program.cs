4998fcf80b242a984aaa768e1d8113b6ece04599
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-05-25 20:59:35 +0200

    connectioooooooooon

--- Auth-Api/Program.cs
+++ Auth-Api/Program.cs
@@ -13,11 +13,30 @@ Env.Load();
 
 // Configure database context
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("DB_CONNECTION_STRING environment variable is not set.");
+}
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 
 // Read the JWT key from environment variables
-var key = Encoding.ASCII.GetBytes(Environment.GetEnvironmentVariable("JWT_KEY"));
+var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY");
+if (string.IsNullOrEmpty(jwtKey))
+{
+    throw new InvalidOperationException("JWT_KEY environment variable is not set.");
+}
+var key = Encoding.ASCII.GetBytes(jwtKey);
+
+// Configure Kestrel to listen on ports 5000 (HTTP) and 5001 (HTTPS)
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.ListenAnyIP(5000); // HTTP
+    serverOptions.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps();
+    });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
