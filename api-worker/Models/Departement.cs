2298f8c28d605c9c1f8c90d0456625f2bef8a4a8
ridha-boughediri <ridha.boughediri@laplateforme.io>
2024-05-26 15:48:22 +0200

    worker departement crud

---
+++ api-worker/Models/Departement.cs
@@ -0,0 +1,9 @@
+namespace api_worker.Modeles
+{
+    public class Departement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ api-worker/Models/Worker.cs
@@ -0,0 +1,10 @@
+namespace api_worker.Modeles
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
